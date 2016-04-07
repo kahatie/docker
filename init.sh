dd0a6521667a243096dfe4c67d19a4bbe00b7a68
kahatie <kahatie@gmail.com>
2016-04-07 09:20:47 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-initToken="/home/.iniToken"
-svnRep="/home/svn/repository"
 
 # deplace workspace mysql dans /home 
 mkdir -p /home/mysql
 mv /var/lib/mysql /home/mysql
 ln -s /home/mysql /var/lib/mysql
 chown -h mysql:mysql /home/mysql
+
 # initialise la base de donnée mysql
-if !([ -f "$initToken" ]) then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
   mysql_install_db –-user=mysql –ldata=/var/lib/mysql
 fi
 
@@ -18,9 +17,9 @@ a2dissite 000-default.conf
 a2ensite default.conf
 
 # creation du depot si il n'existe pas 
-if !([ -f "$svnRep" ]) then
-  mkdir -p "$svnRep"
-  svnadmin create "$svnRep"
-  mv /tmp/hooks "$svnRep/hooks"
+if !([ -f /home/svn/repository ]) then
+  mkdir -p /home/svn
+  svnadmin create /home/svn/repository
+  mv /tmp/hooks /home/svn/repository/hooks
   svn checkout --username system file:///home/svn/repository/
 fi
