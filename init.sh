97332d43b38e26935d6a11a22d89ca0cd82fdf38
kahatie <kahatie@gmail.com>
2016-04-07 04:25:48 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,10 @@
 
+#config mod apache
+a2enmod dav_svn
+a2dissite 000-default.conf
+a2ensite default.conf
+
+# creation du depot
+mkdir /home/svn/
+svnadmin create /home/svn/repository/
+svn checkout --username system file:///home/svn/repository/
